6afd798adccf44549e1a5fcbf468db39e9cdce5c
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-14 20:29:33 +0300

    add View Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
+import {fromLonLat} from 'ol/proj';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+
+        let view = new View({
+            center: fromLonLat([37.6178, 55.7517]),
+            zoom: 6,
+        });
+
+
+        const map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new XYZSource({
+                        url: 'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default//GoogleMapsCompatible_Level8/{z}/{y}/{x}.jpg'
+                    })
+                })
+            ],
+            view: view
+        });
+
+
+        const london = fromLonLat([-0.12755, 51.507222]);
+
+        let button = document.getElementById('pan-to-london')
+//@ts-ignore
+        button.addEventListener('click', () => {
+            view.animate({
+                center: london,
+                duration: 2000,
+            });
+        });
+
+    })
+
+
+    return (
+        <div id='map' className='map' style={{height: '500px', width: '1000px'}}>
+            <button id="pan-to-london">Pan to London</button>
+        </div>
+    );
 }
 
 export default App;
